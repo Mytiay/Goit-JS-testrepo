f14bc53f213de8e2c76edbee0dad5da255d81357
Дмитрий <dimas.savin1990@gmail.com>
2022-09-20 19:06:45 +0200

    addd new file

---
+++ src/index.js
@@ -0,0 +1,24 @@
+// переменные
+let x = 5;
+const age = 10;
+const userName = 'Oleg';
+const message = 'hello world'
+const isOpen = true;
+const shouldConfirm = false;
+// оператор typeoff> для проверки типа переменной!
+
+const type = typeof isOpen;
+
+console.log(type)
+
+// alert блокирует интерпритацию
+// console.log('before');
+// alert('did you know?');
+// console.log('after');
+
+// window.confirm(), window.prompt()
+// const shouldRenew = confirm('take again?')
+// console.log(shouldRenew)
+
+const constanta = prompt('add number');
+console.log(constanta);
