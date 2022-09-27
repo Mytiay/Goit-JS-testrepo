4634373562d04b600ef4f604cf2ec534876b5d0e
Дмитрий <dimas.savin1990@gmail.com>
2022-09-27 15:26:26 +0200

    add

--- src/index.js
+++ src/index.js
@@ -1,24 +1,33 @@
-const totalSpent = 2000;
-let payment = 100;
-let discount = 0;
+// const totalSpent = 2000;
+// let payment = 100;
+// let discount = 0;
 
-if (totalSpent < 100) {
-    console.log('not a partner')
-}
-else if (totalSpent >= 100 && totalSpent <= 1000) {
-    console.log('бронзовый партнер, скидка 2%')
-    discount = 0.02;
-}
-else if (totalSpent >= 1000 && totalSpent <= 5000){
-    console.log('серебряный партнер, скидка 5%')
-    discount = 0.05;
-}
-else if (totalSpent > 5000){
-    console.log('gold партнер, скидка 10%')
-    discount = 0.1;
-}
-else {
-    console.log('not a partner, discount 0%')
-}
+// if (totalSpent < 100) {
+//     console.log('not a partner')
+// }
+// else if (totalSpent >= 100 && totalSpent <= 1000) {
+//     console.log('бронзовый партнер, скидка 2%')
+//     discount = 0.02;
+// }
+// else if (totalSpent >= 1000 && totalSpent <= 5000){
+//     console.log('серебряный партнер, скидка 5%')
+//     discount = 0.05;
+// }
+// else if (totalSpent > 5000){
+//     console.log('gold партнер, скидка 10%')
+//     discount = 0.1;
+// }
+// else {
+//     console.log('not a partner, discount 0%')
+// }
+// console.log(`оформляем заказ на сумму ${payment} со скидкой ${discount * 100}%`)
 
-console.log(`оформляем заказ на сумму ${payment} со скидкой ${discount * 100}%`)
+
+
+const carts = [44, 44, 24, 45, 100, 202];
+let total = 0;
+
+for (const cart of carts) {
+    total = total + cart;
+} 
+console.log('total ', total);
