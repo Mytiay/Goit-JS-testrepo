f850fdf661d80a3010d1c4b31331bb2eb6759278
Дмитрий <dimas.savin1990@gmail.com>
2022-09-28 13:58:04 +0200

    add

--- src/index2.js
+++ src/index2.js
@@ -1,10 +1,17 @@
-const numbers = [1, 5, 8, 9, 12, 4, 15, 27, 30, 18, 11];
-let total = 0;
+// const numbers = [1, 5, 8, 9, 12, 4, 15, 27, 30, 18, 11];
+// let total = 0;
 
-for (let i = 0; i < numbers.length; i += 1){
-    console.log(numbers[i])
-    if (numbers[i] % 2 === 0) {
-     total += numbers[i] ;
-    }
-}
-console.log('total:', total)
+// for (let i = 0; i < numbers.length; i += 1){
+//     console.log(numbers[i])
+//     if (numbers[i] % 2 === 0) {
+//      total += numbers[i] ;
+//     }
+// }
+// console.log('total:', total)
+
+
+// ---------------------split------------------------//
+
+const fruits = 'mango'
+
+console.log(fruits.split(''));
