eb156df5c63ef422aac0cc80f3b3275677688549
Дмитрий <dimas.savin1990@gmail.com>
2022-09-30 20:15:15 +0200

    add

--- src/index3.js
+++ src/index3.js
@@ -1,15 +1,21 @@
 //----------------------for of--------------------//
 
-function filterArray(numbers, value) {
-  const filteredNumbers = [];
+// function filterArray(numbers, value) {
+//   const filteredNumbers = [];
 
-    for (let number of numbers) {
-     console.log(number)
-    if (number > value) {
-      filteredNumbers.push(number);
-    }
-  }
-  return filteredNumbers;
+//     for (let number of numbers) {
+//      console.log(number)
+//     if (number > value) {
+//       filteredNumbers.push(number);
+//     }
+//   }
+//   return filteredNumbers;
+// }
+
+// console.log(filterArray([1, 2, 3, 4, 5], 3));
+
+//-----------------------------function----------------------//
+function multiply(x, y, z) {
+  console.log(`Результат умножения равен ${x * y * z}`);
 }
-
-console.log(filterArray([1, 2, 3, 4, 5], 3));
+multiply(2, 3, 5);
