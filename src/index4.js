87656d25602b0b2a977c73695cff46c1aa790b6b
Дмитрий <dimas.savin1990@gmail.com>
2022-10-01 14:35:21 +0200

    add

--- src/index4.js
+++ src/index4.js
@@ -1,24 +1,48 @@
-const apartment = {
-  imgUrl: "https://via.placeholder.com/640x480",
-  descr: "Spacious apartment in the city center",
-  rating: 4.7,
-  price: 5000,
-  tags: ["premium", "promoted", "top", "trusted"],
-  owner: {
-    name: "Henry Sibola",
-    phone: "982-126-1588",
-    email: "henry.carter@aptmail.com",
+// const apartment = {
+//   imgUrl: "https://via.placeholder.com/640x480",
+//   descr: "Spacious apartment in the city center",
+//   rating: 4.7,
+//   price: 5000,
+//   tags: ["premium", "promoted", "top", "trusted"],
+//   owner: {
+//     name: "Henry Sibola",
+//     phone: "982-126-1588",
+//     email: "henry.carter@aptmail.com",
+//   },
+//   location: {
+//     country: 'UK',
+//     city: 'London',
+//   },
+// };
+
+// // Change code below this line
+// apartment.area = 60;
+// apartment.rooms = 3;
+// apartment.location.country = 'Jamaica';
+// apartment.location.city = 'Kingston';
+
+// console.log(apartment);
+
+
+//----------N2----------//
+
+const bookShelf = {
+  books: ["The Last Kingdom"],
+  getBooks() {
+    return this.books;
   },
-  location: {
-    country: 'UK',
-    city: 'London',
+  addBook(bookName) {
+    this.books.push(bookName);
+  },
+  removeBook(bookName) {
+    const bookIndex = this.books.indexOf(bookName);
+    this.books.splice(bookIndex, 1);
   },
 };
 
-// Change code below this line
-apartment.area = 60;
-apartment.rooms = 3;
-apartment.location.country = 'Jamaica';
-apartment.location.city = 'Kingston';
-
-console.log(apartment);
+console.log(bookShelf.getBooks()); // ["The Last Kingdom"]
+bookShelf.addBook("The Mist");
+bookShelf.addBook("Dream Guardian");
+console.log(bookShelf.getBooks()); // ['The Last Kingdom', 'The Mist', 'Dream Guardian']
+bookShelf.removeBook("The Mist");
+console.log(bookShelf.getBooks()); // ['The Last Kingdom', 'Dream Guardian']
