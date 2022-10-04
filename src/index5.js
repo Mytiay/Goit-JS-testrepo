6bbf4d3ba3f75f982e6d497284480868dd062715
Дмитрий <dimas.savin1990@gmail.com>
2022-10-04 10:44:24 +0200

    add

--- src/index5.js
+++ src/index5.js
@@ -1,12 +1,20 @@
-const emailInputName = "email";
-const passwordInputName = "password";
+// const emailInputName = "email";
+// const passwordInputName = "password";
 
-const credentials = {
-  // Change code below this line
+// const credentials = {
+//   // Change code below this line
 
-[emailInputName]: "henry.carter@aptmail.com",
-[passwordInputName]: "jqueryismyjam",
+// [emailInputName]: "henry.carter@aptmail.com",
+// [passwordInputName]: "jqueryismyjam",
 
-  // Change code above this line
-};
-console.log(emailInputName)
+//   // Change code above this line
+// };
+// console.log(emailInputName)
+
+function multiply(...args) {
+  console.log(args); // массив всех аргументов
+}
+
+multiply(1, 2);
+multiply(1, 2, 3);
+multiply(1, 2, 3, 4);
