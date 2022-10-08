4f1d1ed09a151ae53c386bc12ccc9d5f1db4baf8
Дмитрий <dimas.savin1990@gmail.com>
2022-10-08 11:46:12 -0400

    add

--- src/index6.js
+++ src/index6.js
@@ -3,9 +3,13 @@ const atTheOldToad = {
   updatePotionName(oldName, newName) {
     // Change code below this line
 for (let i = 0; i < this.potions.length; i+=1) {
-  
+  const potion = this.potions[i];
+  if (this.potions[i] === oldName) {
+this.potions.splice(i,1,newName)
+  }
 }
 
     // Change code above this line
   },
 };
+console.log(atTheOldToad);
