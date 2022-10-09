0274f5ddf014d70c57f20bc69deb81bfa93f93d4
Дмитрий <dimas.savin1990@gmail.com>
2022-10-09 19:37:24 -0400

    dell add

--- src/index6.js
+++ src/index6.js
@@ -1,54 +1,25 @@
-// const atTheOldToad = {
-//   potions: ["Speed potion", "Dragon breath", "Stone skin"],
-//   updatePotionName(oldName, newName) {
-//     // Change code below this line
-// for (let i = 0; i < this.potions.length; i+=1) {
-//   const potion = this.potions[i];
-//   if (this.potions[i] === oldName) {
-// this.potions.splice(i,1,newName)
-//   }
-// }
+function printValue(value) {
+  console.log(value);
+}
 
-//     // Change code above this line
-//   },
-// };
-// console.log(atTheOldToad);
+function prettyPrint(value) {
+  console.log("Logging value: ", value);
+}
 
-//----------------new functions--------------------//
-const atTheOldToad = {
-  potions: [
-    { name: "Speed potion", price: 460 },
-    { name: "Dragon breath", price: 780 },
-    { name: "Stone skin", price: 520 },
-  ],
-  // Change code below this line
-  getPotions() {
-    return this.potions;
-  },
-  addPotion(newPotion) {
-    for (const potion of this.potions) {
-    if (potion.name === newPotion.name) {
-      return `Error! Potion ${newPotion.name} is already in your inventory!`;
-    }
+function repeat(n, action) {
+  for (let i = 0; i < n; i += 1) {
+    action(i);
   }
-    this.potions.push(newPotion);
-  },
-  removePotion(potionName) {
-    for (const potion of this.potions) {
-      if (potion.name === potionName) {
-        this.potions.splice(this.potions.indexOf(potion), 1);
-      }
-    }
-     return `Potion ${potionName} is not in inventory!`;
-  },
-  updatePotionName(oldName, newName) {
-     for (const potion of this.potions) {
-      if (potion.name === oldName) {
-        potion.name = newName
-     }
-    }
-     return `Potion ${oldName} is not in inventory!`;
-  },
-  // Change code above this line
-};
-console.log(atTheOldToad);
+}
+
+// Передаем printValue как callback-функцию
+repeat(3, printValue);
+// 0
+// 1
+// 2
+
+// Передаем prettyPrint как callback-функцию
+repeat(3, prettyPrint);
+// Logging value: 0
+// Logging value: 1
+// Logging value: 2
