b59887fed3e28ba2ded3f59090a597193884e0a5
Дмитрий <dimas.savin1990@gmail.com>
2022-10-10 12:25:10 -0400

    add

--- src/index.js
+++ src/index.js
@@ -1,34 +1,12 @@
-// const totalSpent = 2000;
-// let payment = 100;
-// let discount = 0;
+{/* <условие> ? <выражение_если_условие_истинно> : <выражение_если_условие_ложно> </выражение_если_условие_ложно> */}
 
-// if (totalSpent < 100) {
-//     console.log('not a partner')
-// }
-// else if (totalSpent >= 100 && totalSpent <= 1000) {
-//     console.log('бронзовый партнер, скидка 2%')
-//     discount = 0.02;
-// }
-// else if (totalSpent >= 1000 && totalSpent <= 5000){
-//     console.log('серебряный партнер, скидка 5%')
-//     discount = 0.05;
-// }
-// else if (totalSpent > 5000){
-//     console.log('gold партнер, скидка 10%')
-//     discount = 0.1;
-// }
-// else {
-//     console.log('not a partner, discount 0%')
-// }
-// console.log(`оформляем заказ на сумму ${payment} со скидкой ${discount * 100}%`)
+let type;
+const age = 20;
 
-// --------------for of-----------------------//
+if (age >= 18) {
+  type = "adult";
+} else {
+  type = "child";
+}
 
-// const carts = [44, 44, 24, 45, 100, 202];
-// let total = 0;
-
-// for (const cart of carts) {
-//     total = total + cart;
-// }
-// console.log('total ', total);
-
+console.log(type); // "adult"
