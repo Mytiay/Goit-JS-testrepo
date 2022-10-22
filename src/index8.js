3acc79b7f44f82eff2d974fdaa50ee8f3632a29b
Дмитрий <dimas.savin1990@gmail.com>
2022-10-22 18:38:29 -0400

    adddd

---
+++ src/index8.js
@@ -0,0 +1,19 @@
+массив.map((element, index, array) => {
+  // Тело коллбек-функции
+});
+
+массив.flatMap((element, index, array) => {
+  // Тело коллбек-функции
+});
+
+массив.filter((element, index, array) => {
+  // Тело коллбек-функции
+});
+
+массив.find((element, index, array) => {
+  // Тело коллбек-функции
+});
+
+массив.findIndex((element, index, array) => {
+  // Тело коллбек-функции
+});
