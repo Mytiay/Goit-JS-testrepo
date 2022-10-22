cb35c1a5ab1b6a1ebb9bf574af081d8ad0311691
Дмитрий <dimas.savin1990@gmail.com>
2022-10-22 18:40:08 -0400

    add

--- src/index8.js
+++ src/index8.js
@@ -17,3 +17,19 @@
 массив.findIndex((element, index, array) => {
   // Тело коллбек-функции
 });
+
+массив.every((element, index, array) => {
+  // Тело коллбек-функции
+});
+
+массив.some((element, index, array) => {
+  // Тело коллбек-функции
+});
+
+массив.reduce((previousValue, element, index, array) => {
+  // Тело коллбек-функции
+}, initialValue);
+
+const scores = [61, 19, 74, 35, 92, 56];
+scores.sort();
+console.log(scores); // [19, 35, 56, 61, 74, 92]
